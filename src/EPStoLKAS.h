89f1e4adaff044b7677b2ff292c23b31388ccb78
thomas <thomas@reddn.com>
2021-02-26 22:46:23 -0500

    fixed counter bit error

--- src/EPStoLKAS.h
+++ src/EPStoLKAS.h
@@ -48,7 +48,7 @@ void handleEPStoLKAS(){
         if(OPSteeringControlMessageActive){
             switch(EPStoLKASBufferCounter){
                 case 0:
-                    rcvdByte &= B01000000;
+                    rcvdByte &= B00100000;
                     rcvdByte |= steerTorqueModifiedBigValue;
                     break;
                 case 1:
